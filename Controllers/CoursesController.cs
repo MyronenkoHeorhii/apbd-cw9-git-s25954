31c95e9ea600118bb1415178a21fd624af9afa1e
Heorhii Myronenko <s25954@pjwstk.edu.pl>
2026-05-31 23:24:42 +0200

    Implemented the first endpoint

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,6 +1,48 @@
-﻿namespace UniversityTasksDbFirstApi.Controllers;
+﻿using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityTasksDbFirstApi.Data;
+using UniversityTasksDbFirstApi.Models;
+using UniversityTasksDbFirstApi.DTO;
 
-public class CoursesController
+namespace UniversityTasksDbFirstApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CoursesController : ControllerBase
 {
+    private readonly UniversityTasksDbContext _context;
+    
+    public CoursesController(UniversityTasksDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CourseDto>>> CoursesWithAssignmentCounts([FromQuery] bool activeOnly = false)
+    {
+        Console.WriteLine("im here");
+        
+        var query = _context.Courses
+            .AsNoTracking()
+            .AsQueryable();
+
+        if (activeOnly)
+        {
+            query = query.Where(c => c.IsActive);
+        }
+
+        var result = await query
+            .Select(c => new CourseDto
+            {
+                CourseId = c.CourseId,
+                Name = c.Name,
+                Credits = c.Credits,
+                AssignmentCount = c.Assignments.Count()
+            })
+            .ToListAsync();
+
+        return Ok(result);
+    }
     
 }
